193ba79f652f6d67e7a03e00866ed973bb736cbf
ajaxshang <ajaxshang@163.com>
2015-09-17 17:29:10 +0800

    重构页面，添加一些新的UI控件

--- app/src/main/java/com/ajax/demo/activities/MainActivity.java
+++ app/src/main/java/com/ajax/demo/activities/MainActivity.java
@@ -1,6 +1,5 @@
 package com.ajax.demo.activities;
 
-import android.app.ActionBar;
 import android.content.ComponentName;
 import android.content.Intent;
 import android.content.ServiceConnection;
@@ -9,20 +8,29 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.os.IBinder;
 import android.os.Message;
+import android.support.design.widget.CollapsingToolbarLayout;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.app.FragmentTransaction;
-import android.support.v4.view.ViewPager;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
 import android.util.DisplayMetrics;
 import android.util.Log;
 import android.util.TypedValue;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.ajax.demo.R;
+import com.ajax.demo.adapter.DevicesRecyclerViewAdapter;
 import com.ajax.demo.fragments.ElseMenu;
 import com.ajax.demo.fragments.GestureMenu;
 import com.ajax.demo.fragments.HardWareMenu;
@@ -31,12 +39,13 @@ import com.ajax.demo.fragments.ViewMenu;
 import com.ajax.demo.serivices.ReceiveMsgService;
 import com.astuetz.PagerSlidingTabStrip;
 
+import java.util.ArrayList;
+
 /**
  * 上传到GIT进行版本控制
- * */
-public class MainActivity extends FragmentActivity {
+ */
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
-    private FragmentTransaction transaction;
     private FragmentManager manager;
 
     private GestureMenu gestureMenu;
@@ -49,24 +58,38 @@ public class MainActivity extends FragmentActivity {
     ReceiveMsgService receiveMsgService;
 
     private TextView textView;
+    private FragmentTransaction transaction;
+
+    private RecyclerView mRecyclerView;
+    private Toolbar mToolbar;
+    private CollapsingToolbarLayout mCollapsingToolbarLayout;
+    private FloatingActionButton mFab;
+    private DrawerLayout drawerLayout;
+    private DevicesRecyclerViewAdapter mDevicesRecyclerViewAdapter;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        ActionBar actionBar = getActionBar();
-        actionBar.setDisplayHomeAsUpEnabled(false);
+//        ActionBar actionBar = getActionBar();
+//        actionBar.setDisplayHomeAsUpEnabled(false);
         manager = getSupportFragmentManager();
         dm = getResources().getDisplayMetrics();
-        ViewPager pager = (ViewPager) findViewById(R.id.pager);
-        tabs = (PagerSlidingTabStrip) findViewById(R.id.tabs);
-        textView = (TextView) findViewById(R.id.network_state);
-        pager.setAdapter(new PagerAdapter(manager));
-        tabs.setViewPager(pager);
-        setTabsValue();
+//        ViewPager pager = (ViewPager) findViewById(R.id.pager);
+//        tabs = (PagerSlidingTabStrip) findViewById(R.id.tabs);
+//        textView = (TextView) findViewById(R.id.network_state);
+//        pager.setAdapter(new PagerAdapter(manager));
+//        tabs.setViewPager(pager);
+//        setTabsValue();
 
-        bind();
+//        bind();
+
+        setupNavigationView();
+        setupToolbar();
+        setupCollapsingToolbarLayout();
+        setupFab();
+        setupRecyclerView();
     }
 
     private PagerSlidingTabStrip tabs;
@@ -83,6 +106,15 @@ public class MainActivity extends FragmentActivity {
                 TypedValue.COMPLEX_UNIT_DIP, 16, dm));
         tabs.setIndicatorColor(Color.parseColor("#45c01a"));
         tabs.setTabBackground(0);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v.getId() == R.id.fab) {
+//            Snackbar.make(findViewById(R.id.coordinatorLayout), "This is Snackbar",
+//                    Snackbar.LENGTH_LONG).setAction("Action", this).show();
+            Toast.makeText(getApplication(), "先摆设一下", Toast.LENGTH_SHORT).show();
+        }
     }
 
     public class PagerAdapter extends FragmentPagerAdapter {
@@ -145,9 +177,60 @@ public class MainActivity extends FragmentActivity {
         return true;
     }
 
+    private void setupRecyclerView() {
+        mRecyclerView = (RecyclerView) findViewById(R.id.activity_main_recyclerview_numbers);
+        mDevicesRecyclerViewAdapter = new DevicesRecyclerViewAdapter(new ArrayList<String>());
+
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
+        mRecyclerView.setLayoutManager(linearLayoutManager);
+        mRecyclerView.setAdapter(mDevicesRecyclerViewAdapter);
+
+        for (int i = 0; i < 50; i++) {
+            mDevicesRecyclerViewAdapter.getDevices().add("Card Item: " + i);
+        }
+
+        mDevicesRecyclerViewAdapter.notifyDataSetChanged();
+
+
+    }
+
+    private void setupNavigationView() {
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+    }
+
+    private void setupCollapsingToolbarLayout() {
+        mCollapsingToolbarLayout = (CollapsingToolbarLayout) findViewById(R.id.activity_main_collapsing);
+        if (mCollapsingToolbarLayout != null) {
+            mCollapsingToolbarLayout.setTitle("I LOVE ANDROID !!!");
+        }
+    }
+
+    private void setupFab() {
+        mFab = (FloatingActionButton) findViewById(R.id.fab);
+        if (mFab != null)
+            mFab.setOnClickListener(this);
+    }
+
+    private void setupToolbar() {
+        mToolbar = (Toolbar) findViewById(R.id.activity_main_toolbar);
+
+        if (mToolbar != null) {
+            setSupportActionBar(mToolbar);
+        }
+        // Show menu icon
+        final android.support.v7.app.ActionBar ab = getSupportActionBar();
+        ab.setHomeAsUpIndicator(R.drawable.ic_menu);
+        ab.setDisplayHomeAsUpEnabled(true);
+    }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                if (drawerLayout != null)
+                    drawerLayout.openDrawer(GravityCompat.START);
+                return true;
+        }
         return super.onOptionsItemSelected(item);
     }
 
